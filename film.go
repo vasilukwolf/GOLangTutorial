49dac1366b1d0cc02f8ee5ffd4f7990b877a2806
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2026-09-15 20:30:41 +0400

    1 lesson

---
+++ film.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Фильм:", "Интерстеллар\n")
+	fmt.Println("Фантастика:", 2014)
+	fmt.Printf("Рейтинг: %.1f | %d мин |\nРежиссер: %s", 8.6, 169, "Кристофер Нолан")
+}
