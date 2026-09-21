3e7dc370787ce4d86c9c4822f6e29cb7fa7db1fb
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2026-09-21 17:32:57 +0400

    Search film (Начало)

--- cinema_schedule.go
+++ cinema_schedule.go
@@ -1,18 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 var sessions = []string{"10:00", "13:00", "16:00", "19:00"}
 var film_names = []string{"Дюна", "Матрица", "Интерстеллар", "Начало"}
 var halls = []string{"Зал 1", "Зал 2", "Зал 3"}
+
+var halls_counter = 4
 
 func schedule() {
 	for _, hall := range halls {
 		fmt.Print(hall, ":\n")
 		for i := 0; i < len(sessions); i++ {
 			session := sessions[i]
-			film := film_names[i]
-			fmt.Print("  ", session, " - ", film, "\n")
+			film := film_names[rand.Intn(halls_counter)]
+			if film == "Начало" {
+				fmt.Print("Фильм \"Начало\":")
+				fmt.Print("  ", session, "\n")
+				break
+			}
 		}
 	}
 }
