9d63e4b56f75ed9140ace39403703c0960cc5047
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2026-09-24 16:57:19 +0400

    Filtering raiting

---
+++ path2/filterByRating.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func filterByRating(rating []float64, min float64) []float64 {
+	result := []float64{}
+	for _, r := range rating {
+		if r >= min {
+			result = append(result, r)
+		}
+	}
+	return result
+}
+
+func main() {
+	ratings := []float64{4.5, 3.0, 4.0, 5.0, 3.5}
+	minRating := 4.0
+	result := filterByRating(ratings, minRating)
+	fmt.Println(result)
+}
